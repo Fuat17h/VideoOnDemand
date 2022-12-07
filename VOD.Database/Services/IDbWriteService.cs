aaaffa1814267d9e46af9207f99f09221aaed654
Fuat17h <fuat17h@gmail.com>
2022-12-07 16:51:49 +0300

    admin dashboard, btn tag helper, db write and user service

---
+++ VOD.Database/Services/IDbWriteService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Database.Services
+{
+    public interface IDbWriteService
+    {
+        Task<bool> SaveChangesAsync();
+        void Add<TEntity>(TEntity item) where TEntity : class;
+        void Delete<TEntity>(TEntity item) where TEntity : class;
+        public void Update<TEntity>(TEntity item) where TEntity : class;
+
+    }
+}
